0c9e75ffddfe27a5f727b8136ee4c5b06673842b
ryoji12 <ryojiu2003june@icloud.com>
2024-06-30 11:26:16 +0900

    3rd commit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/', function(){
+    return view('posts.index');
+});
+
+Route::get('/', [PostController::class, 'index']);
